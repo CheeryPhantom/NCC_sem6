82b72466d5e193d7e33faf3e81c1ef2f47edaa49
CheeryPhantom <76585267+CheeryPhantom@users.noreply.github.com>
2024-03-10 19:19:29 +0545

    Create GetEmployees.cs

---
+++ MVC/GetEmployees.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC.Models;
+
+namespace MVC
+{
+    public class GetEmployees
+    {
+        static List<EmployeeModel> employees = new List<EmployeeModel>();
+
+        public GetEmployees()
+        {
+            employees.Add(
+                new EmployeeModel { ID = 1, Name = "Bill Gates", Address = "Patan", Dept_ID = 1 }
+            );
+            employees.Add(
+                new EmployeeModel { ID = 2, Name = "Jeff Bezos", Address = "Pokhara", Dept_ID = 2 }
+            );
+        }
+
+        public List<EmployeeModel> GetEmployeeList()
+        {
+            return employees;
+        }
+    }
+}
